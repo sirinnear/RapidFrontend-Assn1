0004c03d31a4d5a42f8683fe53c83165e4a012d8
Sirin Chankao <sirin.cha@student.mahidol.edu>
2021-01-21 01:34:22 +0700

    Valid email error added

--- scripts.js
+++ scripts.js
@@ -3,7 +3,10 @@ var validateEmail = function(email) {
     var pattern = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
     if (pattern.test(email))
     {
-        document.getElementById("placeholder").innerHTML = "";
+        document.getElementById("placeholder").innerHTML = " ";
     }
-    document.getElementById("placeholder").innerHTML = "A valid email is required";
+    else
+    {
+        document.getElementById("placeholder").innerHTML = "A valid email is required";
+    }
 }
