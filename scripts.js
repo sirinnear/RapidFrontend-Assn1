ba179b6c313b9875b2fbdcc265229d85b3219777
Sirin Chankao <sirin.cha@student.mahidol.edu>
2021-01-21 01:13:49 +0700

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+
+var validateEmail = function(email) {
+    var pattern = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if (pattern.test(email))
+    {
+        document.getElementById("placeholder").innerHTML = "";
+    }
+    document.getElementById("placeholder").innerHTML = "A valid email is required";
+}
