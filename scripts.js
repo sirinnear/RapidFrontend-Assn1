7bdb74cbc0c03abff5c54e16bc5d00e3880c5be6
Sirin Chankao <sirin.cha@student.mahidol.edu>
2021-01-21 13:06:33 +0700

    Invalid email error (focused) added

--- scripts.js
+++ scripts.js
@@ -3,10 +3,23 @@ var validateEmail = function(email) {
     var pattern = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
     if (pattern.test(email))
     {
-        document.getElementById("placeholder").innerHTML = " ";
+        return (true);
     }
     else
     {
-        document.getElementById("placeholder").innerHTML = "A valid email is required";
+        return (false);
+    }
+}
+
+var getValidStatus = function() {
+    if (validateEmail(document.getElementById("email").value))
+    {
+        document.getElementById("errorTxt").innerHTML = " ";
+        document.getElementById("email").classList.remove("errorStyle");
+    }
+    else
+    {
+        document.getElementById("errorTxt").innerHTML = "A valid email is required";
+        document.getElementById("email").classList.add("errorStyle");
     }
 }
